849c6c1699d3abedb8298df3bf4872a2276e841c
Alejandro Gómez Poveda <98412537+Prometeus99@users.noreply.github.com>
2025-12-06 11:51:59 +0100

    Asignación del esquema Pydantic de Carrito
    Establecimiento del carrito con Pydantic tanto en la creación como en la lectura.

---
+++ Rares/Biblioteca/app/schemas/carrito.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, ConfigDict, field_validator
+
+class CarritoResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
+    id: int
+    usuario_id: int
+    libro_id: int
+    
+    @field_validator("id")
+    @classmethod
+    def validacion_id(cls, x: int) -> int:
+        if x <= 0:
+            raise ValueError("El ID del carrito debe ser un número positivo.")
+        return x
+
+class CarritoFull(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
+    id:int
+    usuario: UsuarioResponse
+    libro: Libroresponse
+
+class CarritoCreate(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
+    usuario_id: int
+    libro_id: int
