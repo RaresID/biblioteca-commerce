55e91bd989147725364ee47178fd85db65b16db9
Alejandro Gómez Poveda <98412537+Prometeus99@users.noreply.github.com>
2025-12-12 13:35:31 +0100

    Refactor genre schema to user schema with validations

--- Rares/Biblioteca/app/schemas/genero.py
+++ Rares/Biblioteca/app/schemas/genero.py
@@ -1,30 +1,51 @@
-from pydantic import BaseModel, ConfigDict, field_validator
+from pydantic import BaseModel, ConfigDict, field_validator, EmailStr
+from typing import Optional
 
 model_config = ConfigDict(from_attributes=True)
 
-class GenreBase(BaseModel):
-    name: str
+'''Para hacer el esquema de Usuarios es obligatorio
+establecer los validadores para email y la contraseña.'''
 
-class GenreValido(BaseModel):
+class UsuarioValido(BaseModel):
     model_config = model_config
     
-    @field_validator("name")
+    @field_validator("email")
     @classmethod
-    def nombre_valido(cls, a: str | None) -> str | None:
-        if a is None:
+    def validar_email(cls, x: EmailStr | None) -> EmailStr | None:
+        if x is None:
             return None
-        nombre_decidido = a.strip()
-        if not nombre_decidido:
-            raise ValueError("El nombre del género no debe estar vacío.")
-        return nombre_decidido
+        
+        if not x or not str(x).strip():
+            raise ValueError("Si al usuario quieres registrar, el campo del email tendrás que rellenar.")
+        
+        return str(x).strip()
+    
+    @field_validator("password")
+    @classmethod
+    def validar_contrasena(cls, y: str | None) -> str | None:
+        if y is None:
+            return None
+        
+        if len(y) < 8:
+            raise ValueError("La contraseña debe tener como mínio 8 caracteres.")
+        
+        return y
 
-class GenreCreate(GenreBase,GenreValido):
-    pass
+class UsuarioCreate(UsuarioValido):
+    email: EmailStr
+    password: str
 
-class GenreRead(GenreBase):
+class UsuarioResponse(BaseModel):
+    
+    # El campo password no está incluido en esta clase
+    model_config = model_config
+    
     id: int
+    email: EmailStr
 
-    model_config = ConfigDict(from_attributes=True)
-
-class GenreUpdate(GenreCreate):
+class UsuarioUpdate(UsuarioCreate):
     pass
+
+class UsuarioPatch(UsuarioValido):
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
