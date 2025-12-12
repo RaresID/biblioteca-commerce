610e5fe3ab184610107a8b16f484fa54a15ae467
Alejandro Gómez Poveda <98412537+Prometeus99@users.noreply.github.com>
2025-12-12 13:31:13 +0100

    Add relationship for libros in Genre model

--- Rares/Biblioteca/app/models/genero.py
+++ Rares/Biblioteca/app/models/genero.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String
 from app.database import Base
 
@@ -7,3 +7,8 @@ class Genre(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
+    
+    libros = relationship(
+        "Libro",
+        back_populates="genero"
+    )
