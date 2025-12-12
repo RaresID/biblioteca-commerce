5a8d616a315f70a516202cec781865ee336e0a26
Alejandro Gómez Poveda <98412537+Prometeus99@users.noreply.github.com>
2025-12-12 13:34:44 +0100

    Enhance Libro schema with validators and new fields
    Updated the Libro schema to include field validators for title and price, changed price type to float, and added description field.

--- Rares/Biblioteca/app/schemas/libro.py
+++ Rares/Biblioteca/app/schemas/libro.py
@@ -1,27 +1,61 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, field_validator
+from typing import Optional
+
+from genero import GenreRead
+
+model_config = ConfigDict(from_attributes=True)
+
+'''La importación del esquema de genero
+se usará para la clase LibroFull'''
+
+# Se aplicará la clase base con validadores como en UsuarioValido
 
 class LibroBase(BaseModel):
     title: str
-    price: int
-    editor_id: int
+    price: float
+    description: str
     genre_id: int
-    avaible: bool
 
+class LibroValido(BaseModel):
+    model_config = model_config
+    
+    @field_validator("title")
+    @classmethod
+    def titulo_valido(cls, a: str | None) -> str | None:
+        if a is None:
+            return None
+        
+        titulo_decidido = a.strip()
+        if not titulo_decidido:
+            raise ValueError("El campo título no puede estar vacío.")
+        return titulo_decidido
+    
+    @field_validator("price")
+    @classmethod
+    def precio_valido(cls, b: float | None) -> float | None:
+        if b is None:
+            return None
+        
+        if b <= 0.00:
+            raise ValueError("El precio debe ser un número positivo.")
+        return b
 
-class LibroCreate(LibroBase):
+class LibroCreate(LibroBase, LibroValido):
     pass
 
-
-class LibroUpdate(BaseModel):
-    title: str | None = None
-    price: int | None = None
-    editor_id: int | None = None
-    genre_id: int | None = None
-    avaible: bool | None = None
-
-
 class LibroRead(LibroBase):
+    model_config = ConfigDict(from_attributes=True)
     id: int
 
-    model_config = ConfigDict(from_attributes=True)
-    
+class LibroFull(LibroRead):
+    genero: GenreRead
+
+class LibroUpdate(BaseModel):
+    pass
+
+class LibroPatch(LibroValido):
+    title: Optional[str] = None
+    price: Optional[float] = None
+    description: Optional[str] = None
+    genre_id: Optional[int] = None
+
