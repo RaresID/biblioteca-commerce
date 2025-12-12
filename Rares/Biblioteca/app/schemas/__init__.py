35ff6c7e155ea5a91b23980862341c09a4d52858
Alejandro Gómez Poveda <98412537+Prometeus99@users.noreply.github.com>
2025-12-12 13:32:41 +0100

    Update __init__.py to include new schemas

--- Rares/Biblioteca/app/schemas/__init__.py
+++ Rares/Biblioteca/app/schemas/__init__.py
@@ -2,6 +2,15 @@
 Esquemas Pydantic para validación de datos
 """
 
-from app.schemas.song import SongResponse, SongCreate, SongUpdate, SongPatch
+from app.schemas.carrito import CarritoResponse, CarritoFull, CarritoCreate
+from app.schemas.genero import GenreBase, GenreValido, GenreCreate, GenreRead, GenreUpdate
+from app.schemas.libro import LibroBase, LibroValido, LibroCreate, LibroRead, LibroFull, LibroUpdate, LibroPatch
+from app.schemas.usuario import UsuarioValido, UsuarioCreate, UsuarioResponse, UsuarioUpdate, UsuarioPatch
 
-__all__ = ["SongResponse", "SongCreate", "SongUpdate", "SongPatch"]
+__all__ = ["CarritoResponse", "CarritoFull", "CarritoCreate"
+           , "GenreBase", "GenreValido", "GenreCreate"
+           , "GenreRead", "GenreUpdate", "LibroBase"
+           , "LibroValido", "LibroCreate", "LibroRead"
+           , "LibroFull", "LibroUpdate", "LibroPatch"
+           , "UsuarioValido", "UsuarioCreate", "UsuarioResponse"
+           , "UsuarioUpdate", "UsuarioPatch"]
