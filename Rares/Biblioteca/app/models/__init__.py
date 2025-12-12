01da71c3169ac19c4c42b01feab1b37849d81f99
Alejandro Gómez Poveda <98412537+Prometeus99@users.noreply.github.com>
2025-12-12 13:30:04 +0100

    Update __init__.py to include new models
    Inicialización de los modelos para la Librería.

--- Rares/Biblioteca/app/models/__init__.py
+++ Rares/Biblioteca/app/models/__init__.py
@@ -2,6 +2,9 @@
 Modelos de base de datos (SQLAlchemy)
 """
 
-from app.models.song import Song
+from app.models.carrito import Carrito
+from app.models.genero import Genre
+from app.models.libro import Libro
+from app.models.usuario import Usuario
 
-__all__ = ["Song"]
+__all__ = ["Carrito", "Genre", "Libro", "Usuario"]
