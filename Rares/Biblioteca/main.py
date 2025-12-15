6158c28baf74f4354fd0b8f5957aa5cb8dfc85b5
Alejandro Gómez Poveda <98412537+Prometeus99@users.noreply.github.com>
2025-12-15 09:36:54 +0100

    Update main.py

--- Rares/Biblioteca/main.py
+++ Rares/Biblioteca/main.py
@@ -4,4 +4,4 @@ Punto de entrada de la aplicación
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
