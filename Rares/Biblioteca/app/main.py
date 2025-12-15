d6b78fbf875cb8630ea22e16c6d82038b92ab844
Alejandro Gómez Poveda <98412537+Prometeus99@users.noreply.github.com>
2025-12-15 09:39:16 +0100

    Rename app and add new routers for library features

--- Rares/Biblioteca/app/main.py
+++ Rares/Biblioteca/app/main.py
@@ -7,8 +7,12 @@ from app.database import init_db
 from app.routers.api import router as api_router
 from app.routers.web import router as web_router
 
+from app.routers.api.libro import router as libro_router
+from app.routers.api.genero import router as genero_router
+from app.routers.api.carrito import router as carrito_router
+
 # crea la instancia de la aplicación FastAPI
-app = FastAPI(title="Cancioncitas", version="1.0.0")
+app = FastAPI(title="Librerías Demetrio", version="1.0.0")
 
 # inicializa la base de datos con canciones por defecto
 init_db()
@@ -17,10 +21,13 @@ init_db()
 app.include_router(api_router)
 app.include_router(web_router)
 
+app.include_router(libro_router)
+app.include_router(genero_router)
+app.include_router(carrito_router)
 
 """
 # endpoint raíz
 @app.get("/")
 def home():
-    return {"mensaje": "Bienvenido a la app Cancioncitas"}
+    return {"mensaje": "Bienvenido a la página de Librería Demetrio"}
 """
