11de439a587c859dfea6a10dddf37448450b8d09
Alejandro Gómez Poveda <98412537+Prometeus99@users.noreply.github.com>
2025-12-15 09:42:21 +0100

    Include multiple routers in API initialization

--- Rares/Biblioteca/app/routers/api/__init__.py
+++ Rares/Biblioteca/app/routers/api/__init__.py
@@ -3,7 +3,7 @@ Routers de API REST
 Contiene los endpoints que devuelven datos en JSON
 """
 
-from app.routers.api import songs
+from app.routers.api import carrito,genero,libro,usuario
 
 from fastapi import APIRouter
 
@@ -11,4 +11,7 @@ from fastapi import APIRouter
 router = APIRouter()
 
 # incluir router de songs en router principal
-router.include_router(songs.router)
+router.include_router(carrito.router)
+router.include_router(genero.router)
+router.include_router(libro.router)
+router.include_router(usuario.router)
